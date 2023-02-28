c2f9f1eeff6626bf8acd65039a9d4f9e37479e25
Julio Lopez <juliohl77@gmail.com>
2023-02-28 04:02:01 -0300

    primer escritura en el repositorio

---
+++ app.js
@@ -0,0 +1,84 @@
+//Menu lateral
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+//oculto el menu una vez que selecciono una opción
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+//Creo las barritas internas de las barras identificadas por su id
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
+
+//selecciono todas las barras generales para luego manipularlas
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let photoshop = document.getElementById("photoshop");
+crearBarra(photoshop);
+let ilustrator = document.getElementById("ilustrator");
+crearBarra(ilustrator);
+
+//Ahora voy a guardar la cantidad de barritas que se van a pintar
+//comienzan en -1 porque no tiene ninguna pintada al iniciarse
+let contadores = [-1,-1,-1,-1];
+//esta variable la voy a utilizar de bandera para saber si ya ejecuto la animación
+let entro = false;
+
+//función que aplica las animaciones de la habilidades
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_skills>=300 && entro==false){
+        entro = true;
+        const intervalHtml = setInterval(function(){
+            pintarBarra(html, 16, 0, intervalHtml);
+        },100);
+        const intervalJavascript = setInterval(function(){
+            pintarBarra(javascript, 11, 1, intervalJavascript);
+        },100);
+        const intervalPhotoshop = setInterval(function(){
+            pintarBarra(photoshop, 15, 2, intervalPhotoshop);
+        },100);
+        const intervalIlustrator = setInterval(function(){
+            pintarBarra(ilustrator, 14, 3, intervalIlustrator);
+        },100);        
+    }
+}
+
+//lleno barra con la cantidad indicada
+function pintarBarra(id_barra, cantidad, indice, interval){
+    contadores[indice]++;
+    x = contadores[indice];
+    if(x < cantidad){
+        let elementos = id_barra.getElementsByClassName("e");
+        elementos[x].style.backgroundColor = "#4466cd";
+    }else{
+        clearInterval(interval)
+    }
+}
+
+//detecto el scrolling del mouse para aplicar la animación de la barra
+window.onscroll = function(){
+    efectoHabilidades();
+}
---
+++ proyecto.js
@@ -0,0 +1,7 @@
+function vestirseAcorde(temperatura, color){
+    if (temperatura >= 19){
+        return "Remera " + (color);
+    }else{
+        return "Campera " + (color)
+    }
+}
